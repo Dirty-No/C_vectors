7239fbff9f0c568c94f5208172566c3241c86f7b
smaccary <smaccary@student.42.fr>
2021-05-01 19:07:46 +0200

    rm comment

--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smaccary <smaccary@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 14:13:40 by smaccary          #+#    #+#             */
-/*   Updated: 2021/04/11 18:03:04 by smaccary         ###   ########.fr       */
+/*   Updated: 2021/05/01 19:07:33 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,3 @@ int main(void)
 	free_vector(v);
 	return (0);
 }
-
-/*
-void
-	vect_puts(void **ptr)
-{
-	char *str;
-
-	str = *ptr;
-	puts(str);
-}
-
-int
-	main(int argc, char **argv)
-{
-	t_stackgroup 	*group;
-	t_vector		*output;
-	char			*tmp;
-	int				temp;
-
-	output = new_vector(256, sizeof(char *));
-	for (size_t i = 0; i < 10; i++)
-	{
-		tmp = malloc(10);
-		snprintf(tmp, 9, "fuck %zu", i);
-		vector_append(output, &tmp, 1);
-	}
-	vector_iter(output, vect_puts);
-}
-*/
